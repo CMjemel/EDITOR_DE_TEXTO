963aa21074a6cb5d8100f5056d740fbd2220fe12
juel1127 <joelt6719@gmail.com>
2025-06-11 15:38:03 -0600

    Actualizado

---
+++ Joel_Tarqui/Escribir linea/Escribir_Linea.dart
@@ -0,0 +1,9 @@
+class EditorTexto {
+  Queue<String> cola = Queue<String>(); // líneas pendientes
+  List<String> pila = []; // historial de líneas guardadas
+
+  void escribirLinea(String texto) {
+    cola.addLast(texto); // Agrega la línea escrita a la cola
+    print(' Línea escrita: "$texto"'); // Muestra mensaje de confirmación
+  }
+}
